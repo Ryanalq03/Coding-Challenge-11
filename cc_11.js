db46521c41cb3bb3e82bf5e4fc3465a36cbe54e5
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-02 20:09:39 -0500

    Task 1 - Created Book Class

--- cc_11.js
+++ cc_11.js
@@ -1,1 +1,23 @@
+//Challenge #11
+
+//Task 1 Book Class
+
+//Creates a book class with certain properties
+class Book { 
+    constructor(title, author, isbn, copies) { 
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+    };
+    //Created method to give book details
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}, ISBN: ${this.isbn}, Copies: ${this.copies}`
+    };
+    //Created Method to update stock
+    updateCopies(quantity) { 
+        this.copies += quantity
+    };
+}
+
 
